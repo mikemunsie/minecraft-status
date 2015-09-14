56a818aae45238106c28d442e9a595951ee7bf85
Michael Munsie <mikemunstrocity@gmail.com>
2015-09-14 00:50:37 -0500

    Making things a little clearer.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 global.minecraftStatus = {
   port: "9000",                   // What port would you like this script to run on?
-  serverURL: "123.123.1234",      // IP or url without the port number
-  latestLogFileURL: "",           // URL to latest.log file (if you don't have one, leave it empty)
+  serverURL: "123.123.1234",      // IP or url of the Minecraft server without the port number
+  latestLogFileURL: "",           // URL to Minecraft Server latest.log file (if you don't have one, leave it empty)
 };
